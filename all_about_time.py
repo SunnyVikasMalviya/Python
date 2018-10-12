8cbfe0375f9a448981ecc4c6c07427ed4125252b
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2018-10-12 22:09:20 +0530

    Add files via upload

---
+++ all_about_time.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import time
+
+help(time)
+'''help() function is used to display the information regarding any predefined
+python class, function or a variable'''
+
+
+#The below two lines display the use of help() on an integer type 'x'
+#Un-pound it to see the information on int class
+#x=4
+#help(x) 
