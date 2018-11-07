72f632dd851bf8ab05ebd9aa18ffaf5c6a7165fa
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2018-11-07 20:48:04 +0530

    Update Matplotlib_with_csvFiles.py

--- Data Analysis/Matplotlib_with_csvFiles.py
+++ Data Analysis/Matplotlib_with_csvFiles.py
@@ -6,8 +6,10 @@ import numpy as np
 style.use('ggplot')
 
 x, y = np.loadtxt('graph.csv', unpack = True, delimiter = ',')
-#The file is read till the end because of the unpack attribute
-#x and y are lists that store the values from the file
+#The unpack attribute splits the inputs from the file into parts divided \
+#by the delimiter. MUST unpack the exact same number of columns that will\
+#come from the delimiter that you state.
+#x and y are numpy arrays(not lists) that store the values from the file
 
 plt.plot(x, y)
 plt.title('Not important')
