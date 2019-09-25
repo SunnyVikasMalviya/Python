c8fc6d5165897ce647ccdfc703eb8186a0dcfc65
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2019-09-25 14:00:03 +0530

    Add files via upload

---
+++ Turtle Programming/Square_Inside_Out.py
@@ -0,0 +1,21 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("light blue")
+wn.title("Square Inside Out")
+T = turtle.Turtle()
+T.color("white")
+def sqrfunc(size) :
+    for i in range(28) :
+        T.fd(size)
+        T.left(90)
+        size = size + 5
+
+#sqrfunc(146)
+#sqrfunc(126)
+#sqrfunc(106)
+#sqrfunc(86)
+#sqrfunc(66)
+#sqrfunc(46)
+#sqrfunc(26)
+sqrfunc(6)
