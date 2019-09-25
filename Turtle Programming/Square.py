b3da16ebfbdb0cedb128942723518af5e4e0b169
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2019-09-25 13:53:13 +0530

    Add files via upload

---
+++ Turtle Programming/Square.py
@@ -0,0 +1,12 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("light green")
+wn.title("Square")
+T = turtle.Turtle()
+
+for i in range(4) :
+    T.forward(50)
+    T.right(90)
+
+turtle.done()
