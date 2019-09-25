047fd616c7e7947ca83bb031f9469a9e640adea6
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2019-09-25 14:04:46 +0530

    Add files via upload

---
+++ Turtle Programming/Star.py
@@ -0,0 +1,12 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("light green")
+wn.title("Star")
+T = turtle.Turtle()
+
+for i in range(5) :
+    T.forward(50)
+    T.right(144)
+
+turtle.done()
---
+++ Turtle Programming/Triangle.py
@@ -0,0 +1,12 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("light green")
+wn.title("Triangle")
+T = turtle.Turtle()
+
+for i in range(3) :
+    T.forward(100)
+    T.right(120)
+
+turtle.done()
