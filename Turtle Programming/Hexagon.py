c2a87d17bc107c5d8df3f036e14d94bc64c06a96
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2019-09-25 13:50:30 +0530

    Add files via upload

---
+++ Turtle Programming/Hexagon.py
@@ -0,0 +1,12 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("light green")
+wn.title("Hexagon")
+T = turtle.Turtle()
+
+for i in range(6) :
+    T.forward(100)
+    T.right(60)
+
+turtle.done()
