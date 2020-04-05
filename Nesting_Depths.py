8a1269c9291201da14ba06b53e468347f20d3959
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2020-04-05 11:41:21 +0530

    Add files via upload

---
+++ Nesting_Depths.py
@@ -0,0 +1,31 @@
+class Nesting_Depth(object):
+    def __init__(self, S):
+        self.S = S
+        self.S_ = ''
+        return
+    def add_paren(self):
+        temp = ''
+        for x in self.S:
+            x = int(x)
+            temp = x*'(' + str(x) + x*')'
+            self.S_ += temp
+        return self.S_
+    def remove_xtra_paren(self):
+        while len(''.join(self.S_.split(')('))) < len(self.S_):
+            self.S_ = ''.join(self.S_.split(')('))
+        return self.S_
+    def find_S_(self):
+        self.S_ = self.add_paren()
+        self.S_ = self.remove_xtra_paren()
+        return self.S_
+
+test = int(input())
+obj = []
+for t in range(test):
+    S = input()
+    ob = Nesting_Depth(S)
+    obj.append(ob)
+    
+for t in range(test):
+    S_ = obj[t].find_S_()
+    print("Case #{}: {}".format(t+1, S_))
---
+++ Vestigium.py
@@ -0,0 +1,45 @@
+class mat(object):
+    def __init__(self, n, arr):
+        self.n = n
+        self.arr = arr
+        return
+    def find_k(self):
+        k = 0
+        for i in range(self.n):
+            for j in range(self.n):
+                if i == j:
+                    k += self.arr[i][j]
+        return k
+    def find_r(self):
+        r = 0
+        for i in range(self.n):
+            if len(set(self.arr[i])) != self.n:
+                r += 1
+        return r
+    def find_c(self):
+        c = 0
+        for j in range(self.n):
+            temp = []
+            for i in range(self.n):
+                temp.append(self.arr[i][j])
+            if len(set(temp)) != self.n:
+                c += 1
+        return c
+
+test = int(input())
+obj = []
+for t in range(test):
+    n = int(input())
+    arr = []
+    for i in range(n):
+        temp = input().split(' ')
+        temp = [int(x) for x in temp]
+        arr.append(temp)
+    ob = mat(n, arr)
+    obj.append(ob)
+
+for t in range(test):
+    k = obj[t].find_k()
+    r = obj[t].find_r()
+    c = obj[t].find_c()
+    print("Case #{}: {} {} {}".format(t+1, k, r, c))
